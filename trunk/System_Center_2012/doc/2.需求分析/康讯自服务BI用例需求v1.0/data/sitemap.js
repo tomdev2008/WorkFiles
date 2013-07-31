194668ea04680ab01d0bc868daccfa4452293799
Kiichi77 <kiichi77@outlook.com>
2013-07-31 16:03:11 +0800

    2013-07-31-16-03

--- trunk/System_Center_2012/doc/2.需求分析/康讯自服务BI用例需求v1.0/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E),_(c,F,e,f,g,G),_(c,H,e,f,g,I),_(c,J,e,f,g,K),_(c,L,e,f,g,M),_(c,N,e,f,g,O),_(c,P,e,f,g,Q),_(c,R,e,f,g,S),_(c,T,e,f,g,U),_(c,V,e,f,g,W),_(c,X,e,f,g,Y),_(c,Z,e,f,g,ba),_(c,bb,e,f,g,bc)])]);}; 
-var b="rootNodes",c="pageName",d="Home",e="type",f="Wireframe",g="url",h="Home.html",i="children",j="cloudServer",k="cloudServer.html",l="case",m="case.html",n="pice",o="pice.html",p="service",q="service.html",r="help",s="help.html",t="buyPice",u="buyPice.html",v="flow",w="flow.html",x="login",y="login.html",z="register",A="register.html",B="porducts",C="porducts.html",D="console",E="console.html",F="free_try",G="free_try.html",H="make_sure_order",I="make_sure_order.html",J="order",K="order.html",L="order_detail",M="order_detail.html",N="go_up",O="go_up.html",P="user_manager",Q="user_manager.html",R="update_user",S="update_user.html",T="update_password",U="update_password.html",V="protect_password",W="protect_password.html",X="protect_password_problem",Y="protect_password_problem.html",Z="try_apply",ba="try_apply.html",bb="console_detail",bc="console_detail.html";
-return _creator();
-})();
